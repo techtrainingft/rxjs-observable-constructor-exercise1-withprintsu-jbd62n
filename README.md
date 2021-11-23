6464c3302c95b898d886c3fcc09d8647c0312317
techtrainingft <techtrainingft@gmail.com>
2021-11-23 14:18:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-observable-constructor-exercise1-withprintsu-jbd62n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-observable-constructor-exercise1-withprintsu-jbd62n)
---
+++ index.ts
@@ -0,0 +1,32 @@
+//syntax RxJS 6.x
+import { Observable, fromEvent } from "rxjs";
+
+const btnEmit = document.getElementById("emit");
+const btnError = document.getElementById("error");
+const btnComplete = document.getElementById("complete");
+const btnCancel = document.getElementById("cancel");
+const btnPrint = document.getElementById("print");
+
+btnPrint.onclick = () => console.log(subscription);
+
+const observable = new Observable(function(subs) {
+  subs.next("first value");
+  btnEmit.onclick = function() {
+    subs.next("hello hello");
+  };
+  btnError.onclick = function() {
+    subs.error("i'have a bad feeling about this");
+  };
+  btnComplete.onclick = function() {
+    subs.complete();
+  };
+});
+const subscription = observable.subscribe({
+  next: value => console.log(value),
+  error: error => console.log(error),
+  complete: () => console.log("completed")
+});
+
+btnCancel.onclick = function() {
+  subscription.unsubscribe();
+};
